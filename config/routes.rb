2384e9fe1b0aa0285fc666473ad98bcb90584ea3
Henry K <hkewnarny@gmail.com>
2011-05-31 10:28:41 -0700

    Rm default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+ # get "pages/home"
+ # get "pages/contact"
+ # get "pages/about"
 
-  get "pages/contact"
-  get "pages/about"
+  root :to => 'pages#home'
+
+  match '/contact', :to => 'pages#contact' #maps contact_path to '/contact' and contact_url to FULL path
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
