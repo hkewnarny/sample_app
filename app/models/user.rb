f68baa999caefcb605d19f690f91b764f72c3f21
Henry K <hkewnarny@gmail.com>
2011-06-07 17:42:26 -0700

    Partial following users finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,12 @@ require 'digest'
 class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
+
   has_many :microposts, :dependent => :destroy
-  
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, :presence => true,
@@ -24,14 +28,15 @@ class User < ActiveRecord::Base
 					  :format => { :with => email_regex },
 					  :uniqueness => { :case_sensitive => false }
 	# Automatically create the virtual attribute 'password_confirmation'.
-  	validates :password, :presence     => true,
-                         :confirmation => true,
-                         :length       => { :within => 6..40 }
-    before_save :encrypt_password
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                        :length       => { :within => 6..40 }
+  
+  before_save :encrypt_password
 
-    def has_password?(submitted_password)
-    	encrypted_password == encrypt(submitted_password)
-    end
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
     
  	def self.authenticate(email, submitted_password)
       user = find_by_email(email)
@@ -39,7 +44,7 @@ class User < ActiveRecord::Base
       return user if user.has_password?(submitted_password)
     end
 
-  def self.authenticate_with_salt(id, cookie_salt)
+  def self.authenticate_with_salt(id, cookie_salt) #Listing 12.13 shows cookie_salt replaced with stored_salt
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
@@ -52,6 +57,13 @@ def self.authenticate_with_salt(id, cookie_salt)
 end
 =end
 
+  def following(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
   def feed
     Micropost.where("user_id = ?", id)
   end
